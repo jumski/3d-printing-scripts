d2af7d2b9d3d7903980751eaf541832bbe8e9cfe
Wojtek Majewski <jumski@gmail.com>
2021-04-23 17:16:40 +0200

    Rework

--- procedural-wood-in-js/app.js
+++ procedural-wood-in-js/app.js
@@ -1,26 +1,26 @@
-var Simple1DNoise = function() {
-    var MAX_VERTICES = 256;
-    var MAX_VERTICES_MASK = MAX_VERTICES -1;
-    var amplitude = 1;
-    var scale = 1;
+function Simple1DNoise() {
+    let MAX_VERTICES = 256;
+    let MAX_VERTICES_MASK = MAX_VERTICES -1;
+    let amplitude = 1;
+    let scale = 1;
 
-    var r = [];
+    let r = [];
 
-    for ( var i = 0; i < MAX_VERTICES; ++i ) {
+    for ( let i = 0; i < MAX_VERTICES; ++i ) {
         r.push(Math.random());
     }
 
-    var getVal = function( x ){
-        var scaledX = x * scale;
-        var xFloor = Math.floor(scaledX);
-        var t = scaledX - xFloor;
-        var tRemapSmoothstep = t * t * ( 3 - 2 * t );
+    let getVal = function( x ){
+        let scaledX = x * scale;
+        let xFloor = Math.floor(scaledX);
+        let t = scaledX - xFloor;
+        let tRemapSmoothstep = t * t * ( 3 - 2 * t );
 
         /// Modulo using &#038;
-        var xMin = xFloor % MAX_VERTICES_MASK;
-        var xMax = ( xMin + 1 ) % MAX_VERTICES_MASK;
+        let xMin = xFloor % MAX_VERTICES_MASK;
+        let xMax = ( xMin + 1 ) % MAX_VERTICES_MASK;
 
-        var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
+        let y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
 
         return y * amplitude;
     };
@@ -32,7 +32,7 @@ var Simple1DNoise = function() {
     * @param t The value between the two
     * @returns {number}
     */
-    var lerp = function(a, b, t ) {
+    let lerp = function(a, b, t ) {
         return a * ( 1 - t ) + b * t;
     };
 
@@ -48,36 +48,61 @@ var Simple1DNoise = function() {
     };
 };
 
-var generator = new Simple1DNoise();
-var ratio = 1;
-// generator.setAmplitude(0.5);
-// generator.setScale(0.5);
+function generateLayers({ num }) {
+    let generator = new Simple1DNoise();
+    // generator.setAmplitude(0.5);
+    // generator.setScale(0.5);
 
-layer_height = 0.3;
-prusaslicer = [];
-min_temp = 190;
-temp_range = 40;
+    let layers = [];
 
-for (x = 0; x < 1000; x+=1) {
-    var opacity = generator.getVal(x);
-    document.write("<div class='row' style='opacity: " + opacity + ";'> </div>");
+    for (x = 0; x <= num; x+=1) {
+        let value = generator.getVal(x);
 
-    var z = layer_height * (x+1);
-    var temp = min_temp + (temp_range * opacity);
-
-    zWithPrecision = parseFloat(z).toPrecision(5);
-    tempWithPrecision = parseFloat(temp).toPrecision(5);
-    console.log(zWithPrecision, tempWithPrecision);
-    if (x == 0) {
-        prusaslicer.push("{if layer_z <= " + z + "}M104 S" + temp);
+        let z = layer_height * (x+1);
+        layers.push({
+            z: parseFloat(z).toPrecision(5);
+            value: opacity
+        });
     }
-    else {
-        prusaslicer.push("{elsif layer_z <= " + z + "}M104 S" + temp);
-    }
+
+    return layers;
 }
-prusaslicer.push("{endif}");
 
-console.log(prusaslicer.join("\n"));
+function valueToTemp(value) {
+    let min_temp = 190;
+    let temp_range = 40;
+    let temp = min_temp + (temp_range * value);
 
-// var x = 1;
-// var y = generator.getVal(x);
+    return parseFloat(temp).toPrecision(5);
+}
+
+function layersToHtml(layers) {
+    let lines = layers.map({value} => `<div class='row' style='height: 1px; opacity: ${value};'> </div>`);
+
+    return lines.join("\n");
+}
+
+function layersToCode(layers) {
+    let lines = layers.map(({z, value}, i) => {
+        let temp = valueToTemp(value);
+        if (i == 0) {
+            return `{if layer_z <= ${z}}M104 S${temp}`;
+        }
+        else {
+            return `{elsif layer_z <= ${z}}M104 S${temp}`;
+        }
+    });
+    lines.push("{endif}");
+
+    return lines.join("\n");
+}
+
+let layer_height = 0.3;
+let max_height = 80;
+let layers_per_temp = 1;
+let layers_cnt = Math.ceil(max_height / layers_per_temp / layer_height);
+let layers = generateLayers(layers_cnt, layers_per_temp);
+
+document.write(layersToHtml(layers));
+document.write(`<textarea>${layersToCode(layers)}</textarea>`);
+
